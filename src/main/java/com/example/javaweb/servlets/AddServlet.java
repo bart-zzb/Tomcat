0499a38962fc1cc51c6ae639aceed4c73a5b88c6
bart_zzb <3533272661@qq.com>
2024-06-08 11:35:49 +0800

    2024-06-08 Servlet参数设定字符集

--- src/main/java/com/example/javaweb/servlets/AddServlet.java
+++ src/main/java/com/example/javaweb/servlets/AddServlet.java
@@ -9,6 +9,17 @@ import java.io.IOException;
 public class AddServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //get方式下目前不需要设置编码（基于tomcat8）
+        //如果是get请求发送的中文数据，转码稍微麻烦（tomcat8之前）
+//        String name = request.getParameter("name");
+//        //将字符串打散成字节数据
+//        byte[] bytes = name.getBytes("ISO-8859-1");
+//        //将字节数据按照设定的编码重新组装成字符串
+//        name = new String(bytes, "ISO-8859-1");
+
+        //post方式下，设置编码，防止中文乱码
+        request.setCharacterEncoding("UTF-8");
         String name = request.getParameter("name");
         Integer price = Integer.valueOf(request.getParameter("price"));
         Integer count = Integer.valueOf(request.getParameter("stock"));
